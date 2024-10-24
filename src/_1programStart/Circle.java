ab47635bfdb7922afb280cd7ed48f67011cadd06
onkar-tawade <onkartawade32612@gmail.com>
2024-10-24 09:35:07 +0530

    change in Circle.java

--- src/_1programStart/Circle.java
+++ src/_1programStart/Circle.java
@@ -4,15 +4,15 @@ public class Circle {
 
 	public static void main(String[] args) {
 		//area of circle...
-//
-//		int r=4;float pi=3.14f;float res=pi*r*r;
-//		System.out.println(res);
+
+		int r=4;float pi=3.14f;float res=pi*r*r;
+		System.out.println(res);
 	
 		////area of rectangle...
-//	int l=10,b=10;
-//	System.out.println("Area of rectangle:"+(l*b));
-//	System.out.println("Area of square:"+(l*l));
-//	System.out.println("Area of triangle:"+((l*b)/2));
+	int l=10,b=10;
+	System.out.println("Area of rectangle:"+(l*b));
+	System.out.println("Area of square:"+(l*l));
+	System.out.println("Area of triangle:"+((l*b)/2));
 		
 		
 
